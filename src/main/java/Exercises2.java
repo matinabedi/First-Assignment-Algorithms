fd47770d1049e84de02903c3131c66fdcc8a09a0
matinabedi <matinabedi94@gmail.com>
2024-02-27 11:00:12 +0330

    push tes

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Exercises2 {
 
-    /*
+    /* 
     Given an array of integers nums and an integer target, return indices of the two numbers
     such that they add up to target.
 
